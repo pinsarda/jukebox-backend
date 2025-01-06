30de83dd8d43172a009faf0a37ada04bb1492003
A. Pinsard <a.pinsrd@proton.me>
2025-01-06 20:23:01 +0100

    Automatic swagger documentation

--- src/api/routes.rs
+++ src/api/routes.rs
@@ -1,13 +1,18 @@
-use actix_web::{get, web, HttpResponse, Responder};
+use actix_web::{ HttpResponse, Responder};
 use tokio::task;
+use paperclip::actix::{ api_v2_operation, get, web::{ self, Json }};
 use crate::downloader::download_video;
 
-#[get("/test")]
+#[api_v2_operation]
+#[get("/helloworld")]
+/// Displays a simple hello world
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
+#[api_v2_operation]
 #[get("/download/{id}")]
+/// Downloads video with id id to the Downloads folder
 async fn download(id: web::Path<String>) -> impl Responder {
     let url = "https://www.youtube.com/watch?v=".to_owned() + &id.into_inner();
     task::spawn(download_video(url));
--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,19 @@ mod api;
 mod downloader;
 
 use actix_web::{ App, HttpServer };
+use paperclip::actix::OpenApiExt;
+
 use api::routes::{ hello, download };
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
+            .wrap_api()
             .service(hello)
             .service(download)
+            .with_json_spec_at("/api/spec/v2")
+            .build()
     })
     .bind(("127.0.0.1", 8080))?
     .run()

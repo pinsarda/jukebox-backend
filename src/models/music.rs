830b04ce64f23a496cd40ea89350e5a2c30b985a
A. Pinsard <a.pinsrd@proton.me>
2025-01-10 15:24:27 +0100

    add struct for videos and music metadata

---
+++ src/models/music.rs
@@ -0,0 +1,16 @@
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Queryable, Identifiable, Insertable)]
+pub struct YoutubeVideo {
+    pub id: String,
+    pub url: String,
+    pub title: String,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Queryable, Identifiable, Insertable)]
+pub struct Music {
+    pub id: String,
+    pub title: String,
+    pub album: String,
+    pub production_date: NaiveDateTime,
+    pub artist: String,
+    pub has_lyrics: bool
+}

a59513efb85c20fa4035b424786431e9ae682d50
A. Pinsard <a.pinsrd@proton.me>
2025-02-06 21:37:25 +0100

    Add basic db managment for user registration and implement dotenvy

---
+++ src/models/user.rs
@@ -0,0 +1,21 @@
+use crate::schema::*;
+use diesel::prelude::*;
+use paperclip::actix::Apiv2Schema;
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Serialize, Deserialize, Queryable, Selectable, Apiv2Schema)]
+#[diesel(table_name = users)]
+pub struct User {
+    pub id: i32,
+    pub username: String,
+    pub password: String,
+    pub userdata: String
+}
+
+#[derive(Debug, Serialize, Deserialize, Insertable, Apiv2Schema)]
+#[diesel(table_name = users)]
+pub struct NewUser {
+    pub username: String,
+    pub password: String,
+    pub userdata: String
+}

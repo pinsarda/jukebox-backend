900a2a33933daf195c09d31ca6ebc3635e410da6
A. Pinsard <a.pinsrd@proton.me>
2025-02-07 10:50:37 +0100

    Add initial actix identity support and user login

--- src/api/user.rs
+++ src/api/user.rs
@@ -1,8 +1,9 @@
-use actix_web::{ web::Data, Error };
+use actix_identity::Identity;
+use actix_web::{ web::Data, Error, HttpMessage, HttpRequest, HttpResponse, Responder };
 use paperclip::actix::{ api_v2_operation, get, post, web::{self, Json} };
 use crate::models::user::{ User, NewUser };
 use crate::DbPool;
-use crate::db_handlers::user::create_user;
+use crate::db_handlers::user::{ create_user, get_user };
 
 #[api_v2_operation]
 #[post("/user/signup")]
@@ -12,4 +13,19 @@ async fn signup(pool: Data<DbPool>, new_user: Json<NewUser>) -> Result<Json<NewU
     
     let result = create_user(conn, new_user.into_inner()).unwrap();
     Ok(Json(result))
+}
+
+#[api_v2_operation]
+#[post("/user/login")]
+// Login existing user
+async fn login(pool: Data<DbPool>, request: HttpRequest, new_user: Json<NewUser>) -> Result<Json<User>, Error> {
+
+    let conn = &mut pool.get().unwrap();
+    let user = get_user(conn, new_user.into_inner());
+    
+
+    let username = user.as_ref().unwrap().username.clone();
+
+    Identity::login(&request.extensions(), username.into()).unwrap();
+    Ok(Json(user.unwrap()))
 }
--- src/db_handlers/user.rs
+++ src/db_handlers/user.rs
@@ -1,6 +1,7 @@
 use diesel::RunQueryDsl;
+use diesel::prelude::*;
 use diesel::result::Error;
-use crate::models::user::{ User, NewUser};
+use crate::models::user::{ User, NewUser };
 use crate::DbConnection;
 
 pub fn create_user(conn: &mut DbConnection, new_user: NewUser) -> Result<NewUser, Error> {
@@ -11,4 +12,18 @@ pub fn create_user(conn: &mut DbConnection, new_user: NewUser) -> Result<NewUser
         .execute(conn)
         .expect("Database error when inserting user");
     return Ok(new_user);
+}
+
+pub fn get_user(conn: &mut DbConnection, user_data: NewUser) -> Result<User, Error> {
+    use crate::schema::users::dsl::{username, users};
+
+    let mut items = users
+        .filter(username.eq(&user_data.username))
+        .load::<User>(conn)?;
+
+    if let Some(user) = items.pop() {
+        return Ok(user);
+    }
+
+    Err(Error::NotFound)
 }

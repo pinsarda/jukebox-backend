ee7c0052947abb638203aa5a08b0c8ca816c5462
A. Pinsard <a.pinsrd@proton.me>
2025-02-09 04:17:03 +0100

    Add musics structs

--- src/models/music.rs
+++ src/models/music.rs
@@ -1,16 +1,37 @@
-#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Queryable, Identifiable, Insertable)]
+use crate::schema::*;
+use diesel::prelude::*;
+use paperclip::{actix::Apiv2Schema, v2::serde_json};
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Serialize, Deserialize, Apiv2Schema)]
 pub struct YoutubeVideo {
     pub id: i32,
     pub url: String,
     pub title: String,
 }
 
-#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Queryable, Identifiable, Insertable)]
-pub struct Music {
-    pub id: String,
+#[derive(Debug, Serialize, Deserialize, Queryable, Selectable, Apiv2Schema)]
+#[diesel(table_name = albums)]
+pub struct Album {
+    pub id: i32,
     pub title: String,
-    pub album: String,
-    pub production_date: NaiveDateTime,
-    pub artist: String,
-    pub has_lyrics: bool
+    pub artists_ids: String,
+    description: String
+}
+
+#[derive(Debug, Serialize, Deserialize, QueryableByName, Queryable, Selectable, Apiv2Schema)]
+#[diesel(table_name = musics)]
+pub struct Music {
+    pub id: i32,
+    pub title: String,
+    pub artists_ids: String,
+    pub album_id: i32
+}
+
+#[derive(Debug, Serialize, Deserialize, Queryable, Selectable, Apiv2Schema)]
+#[diesel(table_name = artists)]
+pub struct Artist {
+    pub id: i32,
+    pub name: String,
+    description: String
 }

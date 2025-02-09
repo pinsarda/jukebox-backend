176b6656a772ba1301cf6fd9c11d2089f335a7ea
A. Pinsard <a.pinsrd@proton.me>
2025-02-09 02:40:24 +0100

    run diesel migration

--- src/schema.rs
+++ src/schema.rs
@@ -1,6 +1,35 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    albums (id) {
+        id -> Integer,
+        title -> Text,
+        artists_ids -> Text,
+        description -> Nullable<Text>,
+        youtube_id -> Nullable<Text>,
+    }
+}
+
+diesel::table! {
+    artists (id) {
+        id -> Integer,
+        name -> Text,
+        description -> Nullable<Text>,
+        userdata -> Text,
+    }
+}
+
+diesel::table! {
+    musics (id) {
+        id -> Integer,
+        title -> Text,
+        artists_ids -> Text,
+        album_id -> Text,
+        youtube_id -> Nullable<Text>,
+    }
+}
+
+diesel::table! {
     users (id) {
         id -> Integer,
         username -> Text,
@@ -8,3 +37,10 @@ diesel::table! {
         userdata -> Text,
     }
 }
+
+diesel::allow_tables_to_appear_in_same_query!(
+    albums,
+    artists,
+    musics,
+    users,
+);

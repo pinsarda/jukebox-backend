156656a7df3d44658ec7888a881282df1dd82dfe
Antoine Pinsard <apinsard@hackademint.org>
2025-03-19 14:03:24 +0100

    add utoipa docs for player endpoints

--- src/api/player.rs
+++ src/api/player.rs
@@ -5,7 +5,13 @@ use actix_web::{ get, post, web::{Data, Json}, HttpResponse, Responder };
 
 use crate::{db_handlers::music::get_music_by_id, models::Id, player::Player, DbPool};
 
-#[utoipa::path()]
+#[utoipa::path(
+    request_body = Id,
+    responses(
+        (status = OK),
+        (status = FORBIDDEN)
+    )
+)]
 #[post("/player/add_to_queue")]
 /// Add music to queue
 async fn add_to_queue(_id: Identity, pool: Data<DbPool>, player: Data<Player>, query_data: Json<Id>) -> impl Responder {
@@ -17,7 +23,12 @@ async fn add_to_queue(_id: Identity, pool: Data<DbPool>, player: Data<Player>, q
     HttpResponse::Ok().body("Added music to queue")
 }
 
-#[utoipa::path()]
+#[utoipa::path(
+    responses(
+        (status = OK),
+        (status = FORBIDDEN)
+    )
+)]
 #[post("/player/play")]
 /// Start playback of enqueued music
 async fn play(_id: Identity, player: Data<Player>) -> impl Responder {
@@ -25,7 +36,12 @@ async fn play(_id: Identity, player: Data<Player>) -> impl Responder {
     HttpResponse::Ok().body("Starting playback")
 }
 
-#[utoipa::path()]
+#[utoipa::path(
+    responses(
+        (status = OK),
+        (status = FORBIDDEN)
+    )
+)]
 #[post("/player/pause")]
 /// Pause music playback
 async fn pause(_id: Identity, player: Data<Player>) -> impl Responder {
@@ -33,35 +49,60 @@ async fn pause(_id: Identity, player: Data<Player>) -> impl Responder {
     HttpResponse::Ok().body("Pausing music")
 }
 
-#[utoipa::path()]
+#[utoipa::path(
+    responses(
+        (status = OK),
+        (status = FORBIDDEN)
+    )
+)]
 #[post("/player/stop")]
 /// Stop music playback
 async fn stop() -> impl Responder {
     HttpResponse::Ok().body("Not implemented")
 }
 
-#[utoipa::path()]
+#[utoipa::path(
+    responses(
+        (status = OK),
+        (status = FORBIDDEN)
+    )
+)]
 #[post("/player/next")]
 /// Skip to next music in queue
 async fn next() -> impl Responder {
     HttpResponse::Ok().body("Not implemented")
 }
 
-#[utoipa::path()]
+#[utoipa::path(
+    responses(
+        (status = OK),
+        (status = FORBIDDEN)
+    )
+)]
 #[post("/player/previous")]
 /// Skip to previous music in queue
 async fn previous() -> impl Responder {
     HttpResponse::Ok().body("Not implemented")
 }
 
-#[utoipa::path()]
+#[utoipa::path(
+    responses(
+        (status = OK),
+        (status = FORBIDDEN)
+    )
+)]
 #[post("/player/seek")]
 /// Seek to 
 async fn seek() -> impl Responder {
     HttpResponse::Ok().body("Not implemented")
 }
 
-#[utoipa::path()]
+#[utoipa::path(
+    responses(
+        (status = OK),
+        (status = FORBIDDEN)
+    )
+)]
 #[get("/player/state")]
 /// Get player state
 async fn state() -> impl Responder {
